3226f6d6dbe2457459e783c60490e89993abe6da
Yeongchang Park <hazedayz0223@gmail.com>
2023-05-16 18:22:16 +0900

    first

---
+++ src/main/java/as/mafiaplugin/MafiaPlugin.java
@@ -0,0 +1,17 @@
+package as.mafiaplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MafiaPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+                asdffgas
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
