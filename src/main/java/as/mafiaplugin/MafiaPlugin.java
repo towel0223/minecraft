8d2d8a80ef93bac6c8c776fa6d92d23a50cd0df9
SONG USEUNG <dntmd7898@naver.com>
2023-05-16 19:11:59 +0900

    test

--- src/main/java/as/mafiaplugin/MafiaPlugin.java
+++ src/main/java/as/mafiaplugin/MafiaPlugin.java
@@ -14,6 +14,8 @@ public final class MafiaPlugin extends JavaPlugin {
                                 hiyo
 
                                 test SONG
+
+                                        test random
     }
 
     @Override
