5b0ee8bdd160bfb5f923b1686fabadda68963497
Yeongchang Park <hazedayz0223@gmail.com>
2023-05-20 01:03:01 +0900

    Park

--- src/main/java/as/mafiaplugin/MafiaPlugin.java
+++ src/main/java/as/mafiaplugin/MafiaPlugin.java
@@ -1,19 +1,14 @@
 package as.mafiaplugin;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerToggleSprintEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.EventHandler;
-import org.bukkit.World;
-import org.bukkit.Location;
 
 public final class MafiaPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this,this);
-
+        getCommand("ready").setExecutor(new Song());
         // Plugin startup logic
 
     }
@@ -21,16 +16,7 @@ public final class MafiaPlugin extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin shutdown logic
     }
-    @EventHandler
-    public void onPlayerInteract(PlayerToggleSprintEvent event) {
-        Player player = event.getPlayer();
-        Location eyeLocation = player.getEyeLocation();  // 플레이어의 눈의 위치 가져오기
-        World world = player.getWorld();
-        if(event.isSprinting()) {
-            world.strikeLightning(eyeLocation);  // 번개 생성
-            world.createExplosion(eyeLocation, 5, true);  // 폭발 생성
-        }
-    }
+
 
 
 
--- src/main/java/as/mafiaplugin/Song.java
+++ src/main/java/as/mafiaplugin/Song.java
@@ -5,10 +5,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.Location;
 
-public class Song {
+public class Song implements CommandExecutor {
 
 
-    public class ReadyComplete implements CommandExecutor { //준비완료 기능(지정된 장소 이동,준비 확인 변수 ready
+      //준비완료 기능(지정된 장소 이동,준비 확인 변수 ready
 
         private boolean ready = false;
 
@@ -16,24 +16,21 @@ public class Song {
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
             if (!(sender instanceof Player)) {
                 sender.sendMessage("이 명령어는 플레이어만 사용할 수 있습니다.");
-                return true;
+                return false;
             }
 
             Player player = (Player) sender;
-
-            if (args.length == 1 && args[0].equalsIgnoreCase("준비완료")) {
                 ready = true;
                 player.sendMessage("준비 완료!");
                 Location targetLocation = new Location(player.getWorld(), 207.917, 60.5, 292.932);
                 player.teleport(targetLocation);
                 return true;
-            }
 
-            return false;
+
         }
 
         public boolean isReady() {
             return ready;
         }
-    }
+
 }
