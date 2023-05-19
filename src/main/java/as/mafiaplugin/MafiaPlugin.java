d917807d57c5746f5d2dfda433cb3d8568be97a4
Yeongchang Park <hazedayz0223@gmail.com>
2023-05-20 01:13:18 +0900

    first

--- src/main/java/as/mafiaplugin/MafiaPlugin.java
+++ src/main/java/as/mafiaplugin/MafiaPlugin.java
@@ -8,17 +8,13 @@ public final class MafiaPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this,this);
-<<<<<<< HEAD
+
         getCommand("ready").setExecutor(new Song());
-=======
 
  System.out.println("My first plugin has started!! hello!!");
  getCommand("blockbreak").setExecutor(new Han());
-    }        // Plugin startup logic
 
 
->>>>>>> aab613f8e1c52bd1dd6ddf41f76902be83dc05ab
-        // Plugin startup logic
 
 
 }
