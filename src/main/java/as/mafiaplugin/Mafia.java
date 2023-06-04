be4312e4aafeebafa670d3da09fc13f0e01b8ae6
Yeongchang Park <hazedayz0223@gmail.com>
2023-06-04 09:59:11 +0900

    Mafia_1.00

--- src/main/java/as/mafiaplugin/Mafia.java
+++ src/main/java/as/mafiaplugin/Mafia.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -11,11 +12,7 @@ public class Mafia extends Citizen {
     Mafia() {
         super.job = ChatColor.BLACK + "마피아";
     }
-    @EventHandler
-    public void Sun(PlayerDeathEvent E)
-    {
-        player.sendMessage("당신은 마피아입니다.");
-    }
+
 
 
 }
